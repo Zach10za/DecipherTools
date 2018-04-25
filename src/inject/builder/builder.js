accc7171eace424a4ad68e96200745b81ed13df4
Zach <zach@smithgeiger.com>
2018-04-25 09:56:07 -0700

    Fix new tree items being draggable despite being locked

--- src/inject/builder/builder.js
+++ src/inject/builder/builder.js
@@ -102,7 +102,41 @@ chrome.runtime.sendMessage({type: "enabled"}, function(response) {
 		});
 		// end left side pane lock
 
+		
 
+		var observeDOM = (function(){
+			var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
+				eventListenerSupported = window.addEventListener;
+		
+			return function(obj, callback){
+				if( MutationObserver ){
+					// define a new observer
+					var obs = new MutationObserver(function(mutations, observer){
+						if( mutations[0].addedNodes.length || mutations[0].removedNodes.length )
+							callback();
+					});
+					// have the observer observe foo for changes in children
+					obs.observe( obj, { childList:true, subtree:true });
+				}
+				else if( eventListenerSupported ){
+					obj.addEventListener('DOMNodeInserted', callback, false);
+					obj.addEventListener('DOMNodeRemoved', callback, false);
+				}
+			};
+		})();
+		
+		// Observe a specific DOM element:
+		let currentTreeNodes = 0;
+		observeDOM( leftPane ,function(){ 
+			const tree = document.getElementById('daTree');
+			if (!tree) return;
+			let nodes = tree.childNodes.length;
+			if (currentTreeNodes === nodes) return;
+			currentTreeNodes = nodes;
+			chrome.storage.sync.get(['leftPaneLocked'], (items) => {
+				if (items.leftPaneLocked) lockTree();
+			});
+		});
 
 	}
 	}, 10);
